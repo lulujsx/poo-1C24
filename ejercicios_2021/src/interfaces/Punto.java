3c53ac13c19106021c2310440e42d6e87b840dfb
Luana Vallejos <luanalorenavallejos@gmail.com>
2024-06-19 01:23:26 -0300

    started new interface exercise

---
+++ ejercicios_2021/src/interfaces/Punto.java
@@ -0,0 +1,25 @@
+package interfaces;
+
+public class Punto implements Reseteable {
+	
+	//Atributos
+	private double x;
+	private double y;
+	
+	//Constructor
+	public Punto(double x,double y) {
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ ejercicios_2021/src/interfaces/Reseteable.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Reseteable {
+	// reset() (y cualquier otro metodo)
+	// por defecto es publico y abstracto
+	
+	/*public*/ void reset();
+
+}
