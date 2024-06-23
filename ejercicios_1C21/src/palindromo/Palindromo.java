97f82afb4de44abee876fac53538cb37cd826d33
Luana Vallejos <luanalorenavallejos@gmail.com>
2024-06-23 18:24:44 -0300

    finished stack exercise

--- ejercicios_1C21/src/palindromo/Palindromo.java
+++ ejercicios_1C21/src/palindromo/Palindromo.java
@@ -2,8 +2,31 @@
 //Una palabra es palindromo cuando se lee igual hacia adelante que hacia atras.
 //Por ejemplo: sos,seres,reconocer,somos
 
+
 package palindromo;
+import java.util.*;
 
 public class Palindromo {
-
+	
+	public static void main(String[] args) {
+		Stack<Character> pila = new Stack<Character>();
+		String cadena = "somos";
+		
+		for(int i = 0; i < cadena.length(); i++) {
+			pila.push(cadena.charAt(i));
+		}
+		
+		System.out.println(pila);
+		
+		String desapilada = "";
+		
+		while(!pila.empty()) {
+			desapilada += pila.pop();
+		}
+		
+		System.out.println(desapilada);
+		
+		System.out.println(cadena.equals(desapilada));
+	}
+	
 }
